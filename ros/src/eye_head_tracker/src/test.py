4c6c1834ce58550c658c5efe3a46f18dcdd13e7e
Hollander, Nicholas Hackett <nhhollander@wpi.edu>
2021-04-30 18:55:54 -0400

    Added super basic ROS test node.

--- ros/src/eye_head_tracker/src/test.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-import rospy
-from eye_head_tracker.msg import EyeInfo, EyePoint, FaceInfo
--- ros/src/eye_head_tracker/src/test_node.py
+++ ros/src/eye_head_tracker/src/test_node.py
@@ -15,44 +15,66 @@ Written in 2021 by Nicholas Hollander <nhhollander@wpi.edu>
 import tkinter as tk
 import rospy
 from eye_head_tracker.msg import EyeInfo, FaceInfo, GazeInfo
+import time
 
-ui = None
+data = {
+    'eye': EyeInfo(),
+    'face': FaceInfo(),
+    'gaze': GazeInfo()
+}
+
+def listener():
+    # Ros Stuff
+    rospy.Subscriber('tracker_eyes', EyeInfo, eye_listener)
+    rospy.Subscriber('tracker_face', FaceInfo, face_listener)
+    rospy.Subscriber('tracker_gaze', GazeInfo, gaze_listener)
+    rospy.init_node('test_node', anonymous=True)
+    # Run until keyboard exception or crash
+    rospy.spin()
 
 ##--------------------##
 ## ROS Topic Handlers ##
 ##--------------------##
 
-def eye_listener(data):
-    print(data)
+def eye_listener(data_):
+    data['eye'] = data_
+    print_ui()
 
-def face_listener(data):
-    pass
+def face_listener(data_):
+    data['face'] = data_
 
-def gaze_listener(data):
-    pass
+def gaze_listener(data_):
+    data['gaze'] = data_
 
-class UserInterface(tk.Frame):
+##----------------##
+## User Interface ##
+##----------------##
 
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        # Create widgets
-        
+def print_ui():
+    print("\033[97;44m\033[2J\033[0;0H",end="") # Set appearance and clear screen
+    print("\033[1;97;100m\033[0K \U0001F916 TrackerCore ROS Test Node\033[22m")
+    print("\033[0;97;44m",end="")
+    # Print readings
+    print("\033[1mEyes:\033[22m")
+    print(data['eye'])
+    print("\033[1mFace:\033[22m")
+    print(data['face'])
+    print("\033[1mGaze:\033[22m")
+    print(data['gaze'])
 
 
-def listener():
-    global ui
-    # Ros Stuff
-    rospy.Subscriber('tracker_eyes', EyeInfo, eye_listener)
-    rospy.Subscriber('tracker_face', FaceInfo, face_listener)
-    rospy.Subscriber('tracker_faze', GazeInfo, gaze_listener)
-    rospy.init_node('test_node', anonymous=True)
-    # Create the user interface
-    ui = UserInterface(master=tk.Tk())
+##-------------##
+## Other Stuff ##
+##-------------##
 
 if __name__ == '__main__':
     try:
         listener()
     except rospy.ROSInterruptException:
         pass
+    except KeyboardInterrupt:
+        print("KBE")
+        pass
+    finally:
+        # Clear it out
+        print("\033[033;0m\033[2J\033[0;0H",end="")
